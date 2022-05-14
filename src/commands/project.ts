9f70365f562f55da4d06077808d09aaa5e2186d1
Manu <ustenko.mi@gmail.com>
2022-05-14 21:34:09 +0200

    Add templates list

--- src/commands/project.ts
+++ src/commands/project.ts
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 require('dotenv').config()
 import { Command } from 'commander'
 import octokit from '../utils/Octokit'
+import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types'
 
 export default (command: Command) => {
   const project = command
@@ -21,15 +22,21 @@ export default (command: Command) => {
           GITHUB_TEMPLATES_REPOSITORY,
           GITHUB_TEMPLATES_PATH
         } = process.env
-        const res = await octokit.request(
-          'GET /repos/{owner}/{repo}/contents/{path}',
-          {
+        const res: GetResponseDataTypeFromEndpointMethod<any> =
+          await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
             owner: GITHUB_OWNER,
             repo: GITHUB_TEMPLATES_REPOSITORY,
             path: GITHUB_TEMPLATES_PATH
-          }
-        )
-        console.log(res)
+          })
+        const templates = res.data
+        if (templates) {
+          console.log('Next templates are found:')
+          templates.forEach(({ name }, i) => {
+            console.log(`${++i}: ${name}`)
+          })
+        } else {
+          console.log('Templates not found')
+        }
       } catch (e) {
         console.log(chalk.red('Not possible to get templates'))
         console.log(e)
